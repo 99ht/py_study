47ae0d4d216395ec7d00bd200041623d3adf8abf
Nonoas <1356955215@qq.com>
2024-11-22 12:39:04 +0800

    简化id构建代码

--- stock_quotes/web_crawling.py
+++ stock_quotes/web_crawling.py
@@ -52,8 +52,8 @@ class StockApp:
         tree = ttk.Treeview(root)
         self.tree = tree
 
-        columns = ["#0", "#1", "#2", "#3", "#4", "#5", "#6"]
-        headers = ["序号", "股票代码", "股票名称", "市场代码", "涨跌幅", "当前股价", "当日涨跌"]
+        headers = ["序号", "股票代码", "股票名称", "涨跌幅", "当前股价", "当日涨跌"]
+        columns = [f"#{i}" for i in range(len(headers))]
 
         # 设置列
         tree["columns"] = columns
@@ -68,7 +68,6 @@ class StockApp:
         tree.column("#3", width=60, minwidth=60, anchor=tk.CENTER)
         tree.column("#4", width=60, minwidth=60, anchor=tk.CENTER)
         tree.column("#5", width=60, minwidth=60, anchor=tk.CENTER)
-        tree.column("#6", width=60, minwidth=60, anchor=tk.CENTER)
 
         # 配置标签,设置颜色
         tree.tag_configure('green', foreground='green')
@@ -102,16 +101,18 @@ class StockApp:
         if key not in item_dict:
             item = self.tree.insert("", tk.END, text=str(i),
                                     values=(
-                                        stock_code, name, market_dict[market_code], change, price,
+                                        market_dict[market_code] + stock_code, name, change, price,
                                         change_price),
-                                    tags=('green',) if float(change_price) < 0 else (('red',) if float(change_price) > 0 else ''))
+                                    tags=('green',) if float(change_price) < 0 else (
+                                        ('red',) if float(change_price) > 0 else ''))
             item_dict[key] = item
         else:
             self.tree.item(item_dict[key],
                            values=(
-                               stock_code, name, market_dict[market_code], change, price,
+                               market_dict[market_code] + stock_code, name, change, price,
                                change_price),
-                           tags=('green',) if float(change_price) < 0 else (('red',) if float(change_price) > 0 else ''))
+                           tags=('green',) if float(change_price) < 0 else (
+                               ('red',) if float(change_price) > 0 else ''))
 
     def on_closing(self):
         self.running = False
